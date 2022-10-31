0a01028e0dfa3e3c2ff5096232ec99158e8d0b04
we <reddypallyradhika@gmail.com>
2022-10-31 07:00:33 -0700

    Added:Address Book MySql program:UC1

--- src/main/java/com/bridgeLabz/AddressBook.java
+++ src/main/java/com/bridgeLabz/AddressBook.java
@@ -1,7 +1,17 @@
 package com.bridgeLabz;
 
+/**UC1
+ * Ability to create a
+ * Address Book Service DB
+ * - Use SQL Client to create DB & DB recods
+ */
 public class AddressBook {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book");
+        /*
+        create database  Address_Book_System;
+        show databases;
+        use Address_Book_System;
+         */
     }
 }
